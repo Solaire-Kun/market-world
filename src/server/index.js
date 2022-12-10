a62b38d9e4352d3c918172aa1a5fca896bcc0d49
Solaire-Kun <vinciguerraluca03@hotmail.com>
2022-12-10 23:43:35 +0100

    update

--- src/server/index.js
+++ src/server/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const multer = require('multer');
 require('dotenv/config');
 const server = app.listen(3000);
+
 const WebSocket = require('ws');
 new WebSocket.Server({ server });
 
@@ -18,7 +19,6 @@ const registerRoute = require('./routes/register');
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(express.static('../src'))
 //app.use('/add-product', productsRoute);
 app.use('/login', loginRoute);
 app.use('/register', registerRoute);
@@ -35,14 +35,16 @@ const FileStorage = multer.diskStorage({
 });
 const upload = multer({ storage: FileStorage })
 
-// Connect
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.DB_CONNECTION, () => console.log("Connected!"));
 
 app.post('/upload', upload.array('images', 2), (req, res) => {
     res.json('Images successfully uploaded!').status(201);
 });
 
-app.get('/*', function (request, response){
-    response.sendFile(path.resolve('public', 'index.html'))
+const filePath = path.join('public', 'index.html');
+app.get('*', function (request, response){
+    response.sendFile(path.resolve(filePath))
 });
+
+// Connect
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.DB_CONNECTION, () => console.log("Connected!"));
